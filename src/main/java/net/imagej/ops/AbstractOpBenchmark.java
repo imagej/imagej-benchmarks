0076acf257fa2fdf367ac399674dc4a044404d76
Stefan Helfrich <stefan.helfrich@uni-konstanz.de>
2017-08-01 08:44:30 +0200

    Add benchmark for LocalMeanThreshold op

---
+++ src/main/java/net/imagej/ops/AbstractOpBenchmark.java
@@ -0,0 +1,107 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2014 - 2017 Board of Regents of the University of
+ * Wisconsin-Madison, University of Konstanz and Brian Northan.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imagej.ops;
+
+import net.imagej.ops.OpService;
+import net.imglib2.FinalInterval;
+import net.imglib2.img.array.ArrayImg;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.img.basictypeaccess.array.ByteArray;
+import net.imglib2.type.numeric.integer.ByteType;
+import net.imglib2.util.Intervals;
+
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.TearDown;
+import org.scijava.Context;
+import org.scijava.log.LogService;
+import org.scijava.plugin.Parameter;
+
+public abstract class AbstractOpBenchmark {
+
+	@Parameter
+	protected Context context;
+
+	@Parameter
+	protected OpService ops;
+
+	@Parameter
+	protected LogService logService;
+
+	private int seed;
+
+	private int pseudoRandom() {
+		return seed = 3170425 * seed + 132102;
+	}
+
+	/** Subclasses can override to create a context with different services. */
+	protected Context createContext() {
+		return new Context(OpService.class, LogService.class);
+	}
+
+	/**
+	 * Sets up a SciJava context with {@link OpService} and {@link LogService}.
+	 */
+	@Setup
+	public void setUp() {
+		createContext().inject(this);
+	}
+
+	/**
+	 * Disposes of the {@link OpService} and {@link LogService} that were
+	 * initialized in {@link #setUp()}.
+	 */
+	@TearDown
+	public synchronized void cleanUp() {
+		if (context != null) {
+			context.dispose();
+			context = null;
+			ops = null;
+			logService = null;
+		}
+	}
+
+	public ArrayImg<ByteType, ByteArray> generateByteArrayTestImg(
+		final boolean fill, final long... dims)
+	{
+		final byte[] array = new byte[(int) Intervals.numElements(new FinalInterval(
+			dims))];
+
+		if (fill) {
+			seed = 17;
+			for (int i = 0; i < array.length; i++) {
+				array[i] = (byte) pseudoRandom();
+			}
+		}
+
+		return ArrayImgs.bytes(array, dims);
+	}
+
+}
---
+++ src/main/java/net/imagej/ops/threshold/localMean/LocalMeanThresholdBenchmark.java
@@ -0,0 +1,110 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2014 - 2017 Board of Regents of the University of
+ * Wisconsin-Madison, University of Konstanz and Brian Northan.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imagej.ops.threshold.localMean;
+
+import java.util.concurrent.TimeUnit;
+
+import net.imagej.ops.AbstractOpBenchmark;
+import net.imglib2.algorithm.neighborhood.RectangleShape;
+import net.imglib2.exception.IncompatibleTypeException;
+import net.imglib2.img.Img;
+import net.imglib2.outofbounds.OutOfBoundsMirrorFactory;
+import net.imglib2.outofbounds.OutOfBoundsMirrorFactory.Boundary;
+import net.imglib2.type.logic.BitType;
+import net.imglib2.type.numeric.integer.ByteType;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+/**
+ * Benchmarks {@link LocalMeanThreshold}.
+ * 
+ * @author Stefan Helfrich (University of Konstanz)
+ */
+@State(Scope.Benchmark)
+public class LocalMeanThresholdBenchmark extends AbstractOpBenchmark {
+
+	private Img<ByteType> in;
+	private Img<BitType> out;
+
+	@Param({ "1", "2" })
+	private int spanSize;
+
+	@Param("250")
+	private int imageBorderLength;
+
+	@Setup
+	public void initImg() {
+		in = generateByteArrayTestImg(true, imageBorderLength, imageBorderLength);
+		try {
+			out = in.factory().imgFactory(new BitType()).create(in, new BitType());
+		}
+		catch (IncompatibleTypeException exc) {
+			logService.error(exc);
+		}
+	}
+
+	@Benchmark
+	@BenchmarkMode(Mode.AverageTime)
+	@Warmup(iterations = 5)
+	@Measurement(iterations = 10)
+	@OutputTimeUnit(TimeUnit.MILLISECONDS)
+	@Fork(value = 4)
+	public Img<BitType> testLocalMean() {
+		ops.run(LocalMeanThreshold.class, out, in, new RectangleShape(spanSize,
+			false), new OutOfBoundsMirrorFactory<ByteType, Img<ByteType>>(
+				Boundary.SINGLE), 0.0);
+
+		return out;
+	}
+
+	public static void main(String[] args) throws RunnerException {
+		Options opt = new OptionsBuilder().include(LocalMeanThresholdBenchmark.class
+			.getSimpleName()).warmupIterations(5).measurementIterations(10).forks(1).threads(1)
+			.build();
+
+		new Runner(opt).run();
+	}
+
+}
