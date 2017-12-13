8cf24f17863c3acc5e84c61979ef55db1ace15fc
Stefan Helfrich <stefan.helfrich@uni-konstanz.de>
2017-12-13 13:18:31 +0100

    Update license headers

--- src/main/java/net/imagej/ops/threshold/localMean/LocalMeanThresholdBenchmark.java
+++ src/main/java/net/imagej/ops/threshold/localMean/LocalMeanThresholdBenchmark.java
@@ -2,8 +2,8 @@
  * #%L
  * ImageJ software for multidimensional image processing and analysis.
  * %%
- * Copyright (C) 2014 - 2017 Board of Regents of the University of
- * Wisconsin-Madison, University of Konstanz and Brian Northan.
+ * Copyright (C) 2017 Board of Regents of the University of
+ * Wisconsin-Madison and University of Konstanz.
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
